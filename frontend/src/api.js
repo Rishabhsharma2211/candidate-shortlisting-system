21a3a7e406ffae5b63110d585e3d27baef68d296
Rishabhsharma2211 <rishabhcse2006@gmail.com>
2026-05-17 15:50:37 +0530

    Updated production API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://candidate-shortlisting-system-fm9e.onrender.com/api",
 });
 
 export default API;
